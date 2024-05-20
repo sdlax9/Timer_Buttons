dfd34d332a4ea5981f0ff3a0f675e2378ec9a285
brandonsmith5433 <brandonsmith5433@gmail.com>
2024-05-19 20:55:44 -0700

    stuff

--- Main.py
+++ Main.py
@@ -1,7 +1,17 @@
 from gpiozero import LED
 from gpiozero import Button
+from gpiozero import LEDBoard
 from time import sleep
 
+
+# This code should be the start of the program, will cycle through lights at .3s intervals
+# need to figure out an intro and outro to the loop.  
+# allLED = LEDBoard(17,24,5,20,26)
+# 
+#for led in allLED:
+#    led.on()
+#    sleep(.3)
+#    led.off()
 
 whiteButton = Button(27)
 whiteLED = LED(17)
@@ -39,9 +49,13 @@ redLED.off()
 greenLED.on()
 greenButton.wait_for_press()
 greenLED.off()
+
 blueLED.on()
 blueButton.wait_for_press()
 blueLED.off()
+
 print('complete')
 
+
+def held(whiteButton,yellowButton,redButton,greenButton,blueButton)
 exit
